5826c65c09c9b0b9f2fa19b17a2690d499de1a0f
crook-ed <rajatgoyal6404@gmail.com>
2023-11-06 01:01:05 +0530

    connecting dtatabase link to render.com

--- db.js
+++ db.js
@@ -4,8 +4,9 @@ require('dotenv').config();
 
 const devConfig = new Sequelize(process.env.PG_DATABASE, process.env.PG_USER, process.env.PG_PASSWORD, {
     host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
     dialect: 'postgres',
+    port: process.env.PG_PORT,
+    
   });
 
 
